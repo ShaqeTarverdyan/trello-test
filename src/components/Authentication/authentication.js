d29ee48f063e3bebf892afe7f103a98479b0cc2f
shaqe <tshaqe@gmail.com>
2020-05-27 01:16:43 +0400

    Create Authentication components

---
+++ src/components/Authentication/authentication.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+import SignIn from "./signIn";
+import SignUp from "./signUp";
+
+const Page = () => {
+  const [activeTab, setActiveTab] = useState("");
+  const toggle = (value) => {
+    setActiveTab(value);
+  };
+  return (
+    <div>
+      <h3>Authentication component</h3>
+      <div>
+        <button onClick={() => toggle("login")}>Log in</button>
+        <button onClick={() => toggle("register")}>Register</button>
+      </div>
+      <div>
+        {activeTab === "login" && <SignIn />}
+        {activeTab === "register" && <SignUp />}
+        {activeTab === "" && <div>Hello </div>}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
