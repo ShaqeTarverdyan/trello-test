4cae7ec6c116c628473e1cf9038efbcec6c2b2de
shaqe <tshaqe@gmail.com>
2020-05-29 02:54:54 +0400

    create reducer for auth

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,5 +1,24 @@
-const initialState = {};
+import * as actions from "../actions/actionTypes";
+
+const initialState = {
+  loading: false,
+  errorMessage: null,
+};
 
 export default (state = initialState, action) => {
-  return "dwadwadaw";
+  const newState = { ...state };
+  switch (action.type) {
+    case actions.AUTH_START:
+      return { ...newState, loading: true };
+    case actions.AUTH_END:
+      return { ...newState, loading: false };
+    case actions.AUTH_ERROR:
+      return { ...newState, errorMessage: action.payload };
+    case actions.AUTH_SUCCESS:
+      return { ...newState, errorMessage: false };
+    case actions.CLEAN_UP:
+      return { ...newState, errorMessage: null, loading: false };
+    default:
+      return newState;
+  }
 };
