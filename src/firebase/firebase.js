2ca18b824e843665d6460d31bbdafa820addd8b9
shaqe <tshaqe@gmail.com>
2020-05-28 16:03:56 +0400

    Update firebase file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,17 @@
-import * as firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyAS4TwqWIMmG1hM4H2dfYpjhQMKc0DFmz8",
+  apiKey: process.env.REACT_APP_APIKEY,
   authDomain: "trello-2f80a.firebaseapp.com",
   databaseURL: "https://trello-2f80a.firebaseio.com",
   projectId: "trello-2f80a",
   storageBucket: "trello-2f80a.appspot.com",
   messagingSenderId: "164064694937",
 };
+
 firebase.initializeApp(config);
+firebase.firestore();
 
 export default firebase;
