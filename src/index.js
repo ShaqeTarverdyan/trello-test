c99762d82574871662ce0b2407d36f96d0cbea88
shaqe <tshaqe@gmail.com>
2020-05-28 19:34:58 +0400

    1. Add globalStyle in main js file 2.  Add reactReduxFirebas provider in App Component 3.  Wrap  App whit ThemeProvider (provide whit global styles)

--- src/index.js
+++ src/index.js
@@ -2,15 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import theme from "./utils/theme";
+import GlobalStyles from "./utils/globals";
+import firebase from "firebase/app";
 
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
 import { Provider } from "react-redux";
-import store from "./stateManagement/store/index";
+import store from "./store/index";
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
+};
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  // createFirestoreInstance // <- needed if using firestore
+};
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <React.StrictMode>
+            <App />
+            <GlobalStyles />
+          </React.StrictMode>
+        </ThemeProvider>
+      </BrowserRouter>
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById("root")
 );
